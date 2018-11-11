10bb86603e44384081728cc62cf18e8b3196d0bf
panchocosil <panchocosil@users.noreply.github.com>
2018-11-11 17:55:12 -0300

    Add files via upload

---
+++ brute-massdns.sh
@@ -0,0 +1,7 @@
+
+file=$1
+
+for domains in $(cat $file)
+do
+python /root/massdns/scripts/subbrute.py /root/massdns/lists/names_small.txt "$domains" \
+	| massdns -r /root/massdns/lists/resolvers-1000.txt --verify-ip -o S -s 200 -c 1; done
---
+++ massdns-start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# usage: ./massdns-start.sh domains_lists.txt domain-inscope
+# Example: ./massdns-start.sh example-subdomains-list.txt example
+
+file=$1
+name=$2
+
+# Enumerate all existen Subdomains to get more results with massdns
+awk 'BEGIN{FS=OFS="."}           # Set the input and output field separator to a dot
+     {
+        for(i=1;i<NF;i++) {      # Number of domains to print
+          for(j=i;j<NF;j++)      # For each domain element
+            d=d $j OFS;          # d is the domain
+          a[d $NF]               # store it in the array a
+          d=""                   # Reset the domain
+        }
+     }
+     END{
+       for(i in a)               # Loop through each element of the array a
+         print i                 # and print it
+     }' $file > subfinder-clean-"$name".txt;
+
+# Bruteforce all subdomains under subdomains
+bash brute-massdns.sh subfinder-clean-"$name".txt \
+	| cut -f1 -d' ' \
+	| grep -oP  "(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]" \
+        >> massdns-full-"$name".out; ludicrousdns -d -n /root/massdns/lists/resolvers-1000.txt massdns-full-"$name".out \
+	| grep -oP  "(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]" \
+	| egrep "^([^ ]*\.)?"$name"\."  \
+	| awk '!(count[$0]++)' \
+	>> result-NW-"$name".out; \
+ bash dupl.sh result-NW-"$name".out | tee final-result-NW-"$name".out
